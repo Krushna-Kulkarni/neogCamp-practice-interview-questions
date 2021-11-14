f3f4f0ff8928475d0440b47069e3a2663a054212
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2021-11-14 22:23:04 +0530

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+var inputEl = document.querySelector("#input");
+var btnIncreaseFont = document.querySelector("#btn-increase-font-size");
+var btnDecreaseFont = document.querySelector("#btn-decrease-font-size");
+var outputEl = document.querySelector("#output");
+
+
+btnIncreaseFont.addEventListener("click", () => {
+
+})
